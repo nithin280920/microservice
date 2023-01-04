626145c864ac094ffdfd7ff338b2f39a90bcfe65
Nithin-4ScreenMedia <nithin@4screenmedia.com>
2023-01-05 01:23:53 +0530

    # Associations

--- user/index.js
+++ user/index.js
@@ -1,21 +1,18 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
-require('dotenv').config();
+require("dotenv").config();
 
 app.use(express.json());
 
-app.get('/', (req,res)=>{
-    return res.status(200).send({ Message: "User route" });
-    
-})
-const user = require('./src/routes/userRoutes');
+app.get("/", (req, res) => {
+  return res.status(200).send({ Message: "User route" });
+});
+const user = require("./src/routes/userRoutes");
 
 //declaring routes
-app.use('/user', user);
+app.use("/user", user);
 
-
-
-app.listen(port, "localhost", ()=>{
-    console.log(`user running in http://localhost:${port}`);
-})
+app.listen(port, "localhost", () => {
+  console.log(`user running in http://localhost:${port}`);
+});
